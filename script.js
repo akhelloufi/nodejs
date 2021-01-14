1f15a2fd56a602bafc0c7dca01b37529fc001c78
Khelloufi Abdelkarim <khelloufi2006@gmail.com>
2021-01-14 12:18:22 +0000

    server 14 01 2021

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+var http = require('http');
+
 http.createServer(function (req, res) {
-    let i=0
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-  }).listen(8888);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080);
